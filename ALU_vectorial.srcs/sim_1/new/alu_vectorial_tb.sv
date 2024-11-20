12c9b7f0fcf73a4263f0033743c70af1fd82ab79
EduardoHerediaGonzalez <EduardoHerediaGonzalez@gmail.com>
2024-11-19 19:44:02 -0600

    Initial project version.

---
+++ ALU_vectorial.srcs/sim_1/new/alu_vectorial_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/19/2024 07:09:15 PM
+// Design Name: 
+// Module Name: alu_vectorial_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module alu_vectorial_tb(
+
+);
+endmodule
